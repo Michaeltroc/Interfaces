522f3383b95931e80ef73ee365466fa51adf4a1e
MichaelTc <mitrocollazos@unicauca.edu.co>
2020-09-11 18:53:29 -0500

    Interfaces modelo-vista-controlador

---
+++ src/formulario/Formulario.java
@@ -0,0 +1,14 @@
+package formulario;
+
+/**
+ * @author MichaelTc
+ */
+public class Formulario {
+
+    public static void main(String[] args) {
+        
+        Ventana ventana =new Ventana();
+        ventana.setVisible(true);
+    }
+    
+}
